d7437ecdd6882c27959628e1182e7e776b498e2e
Vitkovskiy <vitkovskiy1912@mail.ru>
2025-11-26 21:54:03 +0300

    HW2.3 init

---
+++ src/test/java/PostmanTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void postTest() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("same data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
